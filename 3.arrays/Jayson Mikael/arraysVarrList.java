030afbcd650288c3a00bbc00709d02e7ff96d64d
JaysM32 <71362967+JaysM32@users.noreply.github.com>
2021-03-23 18:03:15 +0700

    Add files via upload

---
+++ 3.arrays/Jayson Mikael/arraysVarrList.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class arraysVarrList {
+
+	public static void main (String []args) {
+		
+		boolean flag = true; int choice; Scanner scan = new Scanner(System.in);
+
+		
+		ArrayList<String> ShopMenu = new ArrayList();
+		
+		
+		ShopMenu.add("Fish and Chips");
+		ShopMenu.add("Steak");
+		ShopMenu.add("Mac and Cheese");
+		
+		
+		
+		while(flag){
+	           System.out.println("\nWhat would you like to do:\n");
+	           System.out.println("1. Add to Menu");
+	           System.out.println("2. Delete item on Menu");
+	           System.out.println("3. Display Menu");
+	           System.out.println("4. Exit");
+	           System.out.println("Choice: ");
+	           choice = scan.nextInt();
+	           switch(choice){
+	               case 1:{
+	                   String insert;
+	                   System.out.print("What Food / Drink item would you like to add ? \n");
+	                   insert = scan.next();
+	                   ShopMenu.add(insert);
+	                   break;
+	               }
+
+	               case 2:{
+	            	   String delete;
+	            	   System.out.println("Items on the Menu are ");
+	            	   System.out.println(ShopMenu);
+	                   System.out.println("what would you like to delete ?");
+	                   delete = scan.next();
+	                   ShopMenu.remove(delete);
+
+	                   break;
+	               }
+
+	               case 3:{
+	                   System.out.println(ShopMenu);
+	                   break;
+
+	               }
+
+	               case 4:{
+	            	   System.out.println("Program Exitted");
+	                   flag = false;
+	               }
+
+	           }
+	       }
+		
+	}
+}
